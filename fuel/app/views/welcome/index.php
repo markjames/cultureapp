ec3fef0020bd896086c37a719636068a4d66ce73
Mark James <mail@mark.james.name>
2011-01-15 14:24:27 +0000

    Added initial HTML5 setup

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -1,40 +1,137 @@
 <!DOCTYPE html>
-<html>
-<head>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]> <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]> <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]> <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:og="http://ogp.me/ns#" xmlns:egms="http://www.esd.org.uk/standards/egms/3.0/egms-schema#" xmlns:ipsv="http://www.esd.org.uk/standards/ipsv/2.00/ipsv-schema#" xmlns:dv="http://rdf.data-vocabulary.org/#" typeof="v:Event">
 	<meta charset="utf-8">
-	<title>Fuel PHP Framework</title>
-	<style type="text/css">
-		body { background-color: #F2F2F2; margin: 45px 0 0 0; font-family: ‘Palatino Linotype’, ‘Book Antiqua’, Palatino, serif; font-size: 18px }
-		#wrapper { width: 740px; margin: 0 auto; }
-		h1 { color: #333333; font: normal normal normal 62px/1em Impact, Charcoal, sans-serif; margin: 0 0 15px 0; }
-		pre { padding: 15px; background-color: #FFF; border: 1px solid #CCC; font-size: 16px;}
-		#footer p { font-size: 14px; text-align: right; }
-		a { color: #000; }
-	</style>
-</head>
-<body>
-	<div id="wrapper">
-		<h1>FUEL</h1>
-		
-		<div id="content">
-			<p>You have successfully installed the Fuel PHP Framework.</p>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="imagetoolbar" content="no">
+
+	<!-- Standard Metadata -->
+	<title>CultureScore</title>
+	<meta name="description" content="Not yet...">
+	<meta name="author" content="Mark James &amp; Andrew Lowther">
+	<meta name="keywords" content="">
+	<meta name="copyright" content="">
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0;">
 	
-			<p>Your default controller, 'Controller_Welcome' is located at:</p>
+	<!-- Dublin Core Metadata -->
+	<meta property="dc:identifier" content="http://cultureapp.dyndns.org" />
+	<meta property="dc:creator" lang="en" content="Mark James &amp; Andrew Lowther">
+	<meta property="dc:created" lang="en" content="2011-01-15">
+	<meta property="dc:modifed" lang="en" content="2011-01-15">
+	<meta property="dc:format" lang="en" content="text/html">
+	<meta property="dc:language" content="en">
+	<meta property="dc:coverage" lang="en" content="UK">
+	<meta property="dc:publisher" lang="en" content="Mark James" />
+	<meta property="dc:rights" lang="en" content="App &copy; 2011 - Data &copy; respective partners">
 
-			<pre><code>APPPATH/classes/controller/welcome.php</code></pre>
+	<!-- Navigation -->
+	<link rel="home" href="/">
 
-			<p>The Controller_Welcome is using the view:</p>
-			
-			<pre><code>APPPATH/views/welcome/index.php</code></pre>
+	<!-- Icons -->
+	<link rel="shortcut icon" href="/favicon.ico">
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png"><!-- 129x129 -->
+
+	<!-- base -->
+	<base href="">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="assets/css/html5kit/style.css">
+	<link rel="stylesheet" href="assets/css/site/style.css">
+
+	<!-- Analytics -->
+	<script>
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-20765684-1']);
+		_gaq.push(['_trackPageview']);
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+
+	<!-- Fonts -->
+	<script type="text/javascript" src="http://use.typekit.com/aui6wyj.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+	<!-- Modernizr -->
+	<script src="assets/scripts/modernizr-1.6.min.js"></script>
+
+</head>
+<body class="page-index template-index section-none environment-dev">
+
+	<div id="container">
+
+		<header id="header">
+			<div id="header-inner">
+
+				<div id="unit-logo" title="Logo" class="unit" role="region">
+					<h1>CultureScore</h1>
+				</div><!-- /#unit-logo -->
+
+			</div><!-- /#header-inner -->
+		</header><!-- /#header -->
 		
-			<p></p>
-		</div>
-		<div id="footer">
-			<p>
-				<a href="http://fuelphp.com">Fuel PHP</a> is released under the MIT license.<br />
-				Executed in {exec_time}s using {mem_usage}mb of memory.
-			</p>
-		</div>
-	</div>
+		<div id="page-content" class="alt">
+
+			<nav id="unit-primary-navigation" role="navigation" title="Main Navigation">
+				<ul>
+					<li class="section-home" accesskey="1"><a href="/">Home</a></li>
+				</ul>
+			</nav><!-- /#unit-primary-navigation -->
+
+			<div id="primary-content" role="main">
+
+				<div id="unit-sample-content" title="Sample Content" class="unit" role="region">
+
+					<h2>Sample Content</h2>
+
+					<!-- p, span, strong -->
+					<p>Lorem <span>ipsum dolor nato</span> sit amet <strong>strong text</strong>, <em>emphasis consectetur adipisicing elit</em>, <a href="http://www.example.com">link</a>, <cite>Citation</cite>, <code>Inline Code</code> , <abbr title="Cascading Style Sheets">CSS</abbr> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					
+					<p>Lorem <span>ipsum dolor nato</span> sit amet <strong>strong text</strong>, <em>emphasis consectetur adipisicing elit</em>, <a href="http://www.example.com">link</a>, <cite>Citation</cite>, <code>Inline Code</code> , <abbr title="Cascading Style Sheets">CSS</abbr> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt id est laborum.</p>
+					
+					<a href="#" class="more">more</a>
+
+				</div><!-- /#unit-sample-content -->
+
+			</div><!-- /#primary-content -->
+
+			<div id="secondary-content" role="complementary">
+				<div id="canvas"></div>
+			</div><!-- /#secondary-content -->
+		</div><!-- /#page-content -->
+
+		<footer id="footer" role="contentinfo">
+			<p><a href="http://twitter.com/markjames">@markjames</a> &amp; <a href="http://twitter.com/andrewlowther">@andrewlowther</a> at Culture Hack Day 2011 (<a href="http://twitter.com/culturehackday">@culturehackday</a>) &mdash; Code: <a href="https://github.com/markjames/cultureapp">githubcom/markjames/cultureapp</a></p>
+		</footer><!-- /#footer -->
+
+	</div><!-- /#container -->
+
+	<!-- jQuery -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<script src="assets/scripts/jquery.metadata.js"></script>
+	<script src="assets/scripts/jquery.address-1.3.1.min.js"></script>
+
+	<!-- Drawing -->
+	<script src="assets/scripts/raphael/raphael-min.js"></script>
+	<script src="assets/scripts/raphael/raphael-graphs.js"></script>
+
+	<!-- Graph layout -->
+	<!-- http://arborjs.org/ -->
+
+	<!-- Site scripts -->
+	<script src="assets/scripts/site/site.js"></script>
+
+	<!--[if lt IE 7 ]>
+	<script src="assets/scripts/dd_belatedpng.js?"></script>
+	<script>DD_belatedPNG.fix('');</script>
+	<![endif]-->
+
 </body>
 </html>
