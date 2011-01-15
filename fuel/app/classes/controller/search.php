f4268738eec98731514842c5499a689aecb0e9f7
AndrewLowther <andy.lowther@mademedia.co.uk>
2011-01-15 12:27:35 +0000

    Adding search controller and initial empty template.

---
+++ fuel/app/classes/controller/search.php
@@ -0,0 +1,11 @@
+<?php
+
+class Controller_Search extends Controller {
+	
+	public function action_index() {
+		$this->render('search/index');
+	}
+	
+}
+
+?>
---
+++ fuel/app/logs/2011/01/15.php
@@ -0,0 +1,16 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Error - 2011-01-15 11:52:24 --> 2048 - call_user_func() expects parameter 1 to be a valid callback, non-static method Oil\Generate::model() should not be called statically in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/cli.php on line 50
+Error - 2011-01-15 11:52:24 --> 2048 - Non-static method Oil\Generate::write() should not be called statically in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 101
+Error - 2011-01-15 11:52:24 --> 2048 - Non-static method Oil\Generate::migration() should not be called statically in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 109
+Error - 2011-01-15 11:52:24 --> 2048 - Non-static method Oil\Generate::_build_migration() should not be called statically in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 196
+Error - 2011-01-15 11:52:24 --> 8 - Undefined offset: 1 in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 272
+Error - 2011-01-15 11:52:24 --> 8 - Undefined offset: 2 in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 273
+Error - 2011-01-15 11:52:24 --> 8 - Undefined index:  in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 295
+Error - 2011-01-15 11:52:24 --> 2048 - Non-static method Oil\Generate::_find_migration_number() should not be called statically in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 355
+Error - 2011-01-15 11:52:24 --> 2048 - Only variables should be passed by reference in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 375
+Error - 2011-01-15 11:52:24 --> 2048 - Non-static method Oil\Generate::write() should not be called statically in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 363
+Error - 2011-01-15 11:52:24 --> 2048 - Non-static method Oil\Generate::_update_current_version() should not be called statically in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 365
+Error - 2011-01-15 11:52:24 --> 2048 - Non-static method Oil\Generate::write() should not be called statically in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 395
+Error - 2011-01-15 11:53:04 --> 2048 - call_user_func() expects parameter 1 to be a valid callback, non-static method Oil\Generate::model() should not be called statically in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/cli.php on line 50
+Error - 2011-01-15 11:53:04 --> 2048 - Non-static method Oil\Generate::write() should not be called statically in /Users/lowther/Sites/cultureapp/fuel/packages/oil/classes/generate.php on line 101
