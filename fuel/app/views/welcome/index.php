ffaa548a26db599454793783d2a8adf5d96f4b97
AndrewLowther <andy.lowther@mademedia.co.uk>
2011-01-16 11:52:39 +0000

    Moving the luvvie alarm.

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -18,9 +18,9 @@
 
 			<?php include dirname(__file__) . "/../includes/tools.php"; ?>
 
+			<div id="luvvie-alarm"></div>
+			
 			<div id="primary-content" class="" role="main">
-				
-				<div id="luvvie-alarm"></div>
 
 				<div id="calculating" class="tk-bravia">Calculating&hellip;</div>
 
